2c7fd690b3fc50a41c6f3b67e8bba96c02c1a0b9
Tony Nguyễn <tonynguyen@saomocmedia.com>
2019-10-03 19:04:07 +0700

    Buổi 1

---
+++ Buổi 1/demo.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	
+	if(num > 0)
+		{
+			printf("Day la so duong\n");
+		}
+		else
+		{
+			printf("day la so am\n");
+		}
+	/* code */
+	return 0;
+}

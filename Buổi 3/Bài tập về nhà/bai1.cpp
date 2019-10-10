8c9389169410e8cd92da8d6ac898d91a4577a5fe
Tony Nguyễn <tonynguyen@saomocmedia.com>
2019-10-10 17:52:32 +0700

    Fix bug bài 1

--- Buổi 3/Bài tập về nhà/bai1.cpp
+++ Buổi 3/Bài tập về nhà/bai1.cpp
@@ -5,15 +5,13 @@ int main()
 	printf("Nhap so thu nhat:\n");
 	scanf("%d",&a);
 	printf("Nhap so thu hai:\n");
-	scanf("%d",&a);
+	scanf("%d",&b);
 	if(a>b){
-		printf("%d\n",a);
-	}else{
-		if(a<b){
-			printf("%d\n",b);
-		}else{
-
-		}
+		printf("So lon hon la: %d\n",a);
+	}else if(a<b){
+		printf("So lon hon la: %d\n",b );
+	}else if(a==b){
+		printf("2 so bang nhau\n");
 	}
 	return 0;
 }

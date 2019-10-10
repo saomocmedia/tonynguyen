4c0187220bcc13b38ca6595f4fac01c67a59be52
Tony Nguyễn <tonynguyen@saomocmedia.com>
2019-10-10 18:40:45 +0700

    Chua bai 4

--- Buổi 3/Bài tập về nhà/bai4.cpp
+++ Buổi 3/Bài tập về nhà/bai4.cpp
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
+	int a,b,c;
+	printf("Nhap a\n");
+	scanf("%d",&a);
+	printf("Nhap b\n");
+	scanf("%d",&b);
+	printf("Nhap c\n");
+	scanf("%d",&c);
+	if(a==0){
+		if(b==0){
+			if(c==0){
+				printf("Phuong trinh vo so nghiem\n");
+			}else{
+				printf("phuong trinh vo nghiem\n");
+			}
+		}else{
+			printf("nghiem la: %f\n",(float)-c/b );
+		}
+	}else{
+		int D;
+		D = b*b - 4*a*c;
+		if(D>0){
+			float x1 = (float)(-b+sqrt(D))/(2*a);
+			float x2 = (float)(-b-sqrt(D))/(2*a);
+			printf("nghiem thu nhat: %f\n",x1);
+			printf("nghiem thu hai: %f\n",x2);
+		}else if(D==0){
+			printf("hai nghiem cua phuong trinh %f\n",(float)-b/(2*a));
+		}else{
+			printf("Phuong trinh vo nghiem\n");
+		}
+	}
 	return 0;
 }
